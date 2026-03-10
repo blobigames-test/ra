13cd044d501f3bb187478c6bab02d82ff048354e
blobigames-test <ivan.boi.alt2@gmail.com>
2026-03-10 10:11:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const fetch = require('node-fetch');
 
 const app = express();
+const BASE = 'https://files.crazygames.com/ragdoll-archers/41';
 
-app.use('/', createProxyMiddleware({
-  target: 'https://files.crazygames.com/ragdoll-archers/41',
-  changeOrigin: true,
-  pathRewrite: { '^/': '/' }
-}));
+app.get('*', async (req, res) => {
+  try {
+    const url = BASE + req.path;
+    const response = await fetch(url, { redirect: 'follow' });
+    const contentType = response.headers.get('content-type');
+    res.set('Content-Type', contentType);
+    const buffer = await response.buffer();
+    res.send(buffer);
+  } catch (e) {
+    res.status(500).send('Error: ' + e.message);
+  }
+});
 
 app.listen(process.env.PORT || 3000);
